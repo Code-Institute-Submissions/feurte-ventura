4d5a6a2697be532358633e9b6f79f3369f056905
bubbly16 <littlebaz16@hotmail.com>
2020-09-15 15:31:09 +0000

    added details on homepage and also started to add google maps api for popular cities

---
+++ js/popular-cities-maps.js
@@ -0,0 +1,9 @@
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("corralejo-map"), {
+    zoom: 15,
+    center: {
+      lat: 28.730549,
+      lng: -13.868915
+    },
+  });
+}
