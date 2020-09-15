2b0faf530117539311380b041eb9ddca827dbf7b
bubbly16 <littlebaz16@hotmail.com>
2020-09-15 23:19:59 +0000

    added js email funtion with consumer details and send email js file

---
+++ js/sendEmail.js
@@ -0,0 +1,15 @@
+function sendMail(requestForm) {
+    emailjs.send("bubbly16","holiday-request", {
+        "from_name": requestForm.name.value,
+        "from_phone_num": requestForm.phone-num.value,
+        "from_email": requestForm.email.value,
+        "holiday_request": requestForm.holidayrequest.value
+    })
+    .then(
+        function(response) {
+            console.log("CONGRATULATIONS", response);
+        },
+        function(error) {
+            console.log("OOPS SOMETHING WENT WRONG", error);
+        });
+}
